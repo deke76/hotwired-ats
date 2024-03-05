5028c886e6f1da0b1fdfb5b4e530ea0a2d0cf51d
Darcy <darcy.lauder@shaw.ca>
2024-03-04 21:58:03 -0700

    Chapter 1 work through.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Enable web console for home network debugging
+  config.web_console.whitelisted_ips = '192.168.0.1/16'
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
