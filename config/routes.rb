93e4b5e75867a51be0f34c5de615d2e29d7a4203
Darcy <darcy.lauder@shaw.ca>
2024-03-20 22:10:59 -0600

    Devise installed and working in basic flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'dashboard/show'
 
+  authenticated :user do
+    root to: 'dashboard#show', as: :user_root
+  end
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
 end
