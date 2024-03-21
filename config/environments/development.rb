f33a79f43b12887d6d59f8430fb57ba0c93bfe8c
Darcy <darcy.lauder@shaw.ca>
2024-03-20 18:14:33 -0600

    Devise installed and views created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Enable web console for home network debugging
-  config.web_console.whitelisted_ips = '192.168.0.1/16'
+  config.web_console.allowed_ips = '192.168.0.1/16'
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -73,4 +73,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = {
+    host: 'localhost', port: 3000
+  }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'dashboard/show'
-  root to: 'dashboard#show'
+
 end
