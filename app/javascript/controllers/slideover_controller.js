97b8bc5f175808be2c6dfb2ddfb12fa11c0c1dbd
Darcy <darcy.lauder@shaw.ca>
2024-03-31 12:42:27 -0600

    Slideover working

--- app/javascript/controllers/slideover_controller.js
+++ app/javascript/controllers/slideover_controller.js
@@ -1,7 +1,50 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from 'stimulus'
 
-// Connects to data-controller="slideover"
 export default class extends Controller {
+  static targets = [ "slideover" ]
+
   connect() {
+    console.log("Connecting Slideover")
+    this.backgroundHtml = this.backgroundHTML()
+    this.visible = false
+  }
+
+  disconnect() {
+    if (this.visible) {
+      this.close()
+    }
+  }
+
+  open() {
+    console.log("Opening Slideover")
+    this.visible = true
+    document.body.insertAdjacentHTML('beforeend', this.backgroundHtml)
+    this.background = document.querySelector(`#slideover-background`)
+    this.toggleSlideover()
+    document.addEventListener("submit:success", () => {
+      this.close()
+    }, { once: true })
+  }
+
+  close() {
+    this.visible = false
+    this.toggleSlideover()
+    if (this.background) {
+      this.background.classList.remove("opacity-100")
+      this.background.classList.add("opacity-0")
+      setTimeout(() => {
+        this.background.remove()
+      }, 500);
+    }
+  }
+
+  toggleSlideover() {
+    this.slideoverTarget.classList.toggle("right-0")
+    this.slideoverTarget.classList.toggle("-right-full")
+    this.slideoverTarget.classList.toggle("lg:-right-1/3")
+  }
+
+  backgroundHTML() {
+    return `<div id="slideover-background" class="fixed top-0 left-0 w-full h-full z-20"></div>`;
   }
 }
