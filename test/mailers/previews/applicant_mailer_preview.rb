16b1135c9115c5fe273ae8b9a4963afb65a5f5c3
Darcy <darcy.lauder@shaw.ca>
2024-04-13 20:24:52 -0600

    Email sending completed

---
+++ test/mailers/previews/applicant_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/applicant_mailer
+class ApplicantMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/applicant_mailer/contact
+  def contact
+    ApplicantMailer.contact
+  end
+
+end
