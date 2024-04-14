1675a2cd8eb0b7257665f239fa922b27f0a48fd4
Darcy <darcy.lauder@shaw.ca>
2024-04-14 12:38:17 -0600

    Troubleshooting Notifications setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :emails, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   belongs_to :account
   accepts_nested_attributes_for :account
 
