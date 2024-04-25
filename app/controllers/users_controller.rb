3d6627d55f80a70763c0407db753cfaee27ab003
Darcy <darcy.lauder@shaw.ca>
2024-04-24 23:32:51 -0600

    Editing users complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     html = render_to_string(partial: 'form', locals: { user: @user })
-    render operations: cable_car
+    render cable_ready: cable_car
       .inner_html('#slideover-content', html: html)
       .text_content('#slideover-header', text: 'Add a new user')
   end
@@ -21,12 +21,12 @@ class UsersController < ApplicationController
 
     if @user.save
       html = render_to_string(partial: 'user', locals: { user: @user })
-      render operations: cable_car
+      render cable_ready: cable_car
         .prepend('#users', html: html)
         .dispatch_event(name: 'submit:success')
     else
       html = render_to_string(partial: 'form', locals: { user: @user })
-      render operations: cable_car
+      render cable_ready: cable_car
         .inner_html('#user-form', html: html), status: :unprocessable_entity
     end
   end
@@ -34,9 +34,18 @@ class UsersController < ApplicationController
   def edit; 
   end
 
-  def update; end
+  def update
+    set_name
+    if @user.update(user_params.except(:name))
+      render(@user)
+    else
+      render partial: 'edit_form', locals: { user: @user }
+    end
+  end
 
-  def destroy; 
+  def destroy
+    @user.destroy
+    render cable_ready: cable_car.remove(selector: dom_id(@user))
   end
 
   private
@@ -44,8 +53,14 @@ class UsersController < ApplicationController
   def set_user
     @user = User.find(params[:id])
   end
-
+  
+  def set_name
+    first_name, last_name = user_params[:name].split(' ', 2)
+    @user.first_name = first_name
+    @user.last_name = last_name
+  end
+  
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
+    params.require(:user).permit(:first_name, :last_name, :email, :name)
   end
 end
