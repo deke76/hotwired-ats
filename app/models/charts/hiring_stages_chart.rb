66288d502abcd9aa20206887e5940978d7528fe6
Darcy <darcy.lauder@shaw.ca>
2024-05-01 20:37:29 -0600

    Filtering by stage complete

--- app/models/charts/hiring_stages_chart.rb
+++ app/models/charts/hiring_stages_chart.rb
@@ -1,18 +1,18 @@
 class Charts::HiringStagesChart
-  def initialize(account_id)
+  def initialize(account_id, job_id = nil)
     @account_id = account_id
+    @job_id = job_id
   end
 
   def generate
     query_data
   end
 
-  private
-
   def query_data
     Applicant
       .includes(:job)
       .for_account(@account_id)
+      .for_job(@job_id)
       .group('stage')
       .count
   end
