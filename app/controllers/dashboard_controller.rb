1b9424e13cfb0a0446994eb123679ffb2d0f1819
Darcy <darcy.lauder@shaw.ca>
2024-05-01 19:44:43 -0600

    Applicants over time graph completed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    @account = Account.find(current_user.account_id)
+    report_data = Charts::ApplicantsChart.new(current_user.account_id).generate
+
+    @categories = report_data.keys.to_json
+    @series = report_data.values.to_json
   end
 end
