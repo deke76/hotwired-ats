abaff8f640cd725b66937a382d6bb576969953fb
Darcy <darcy.lauder@shaw.ca>
2024-05-01 19:51:26 -0600

    Hiring stages chart added to dashboard

---
+++ app/models/charts/hiring_stages_chart.rb
@@ -0,0 +1,19 @@
+class Charts::HiringStagesChart
+  def initialize(account_id)
+    @account_id = account_id
+  end
+
+  def generate
+    query_data
+  end
+
+  private
+
+  def query_data
+    Applicant
+      .includes(:job)
+      .for_account(@account_id)
+      .group('stage')
+      .count
+  end
+end
